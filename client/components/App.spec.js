e84a2848d44d00376d843706a68ec4dbdcf69ddd
Artem Govorov <artem.govorov@gmail.com>
2018-01-19 19:51:08 +1000

    Update App.spec.js

--- client/components/App.spec.js
+++ client/components/App.spec.js
@@ -1,34 +1,37 @@
 import {expect} from 'chai';
-
-import {tester} from 'sheep';
-
-// In wallaby this also isn't being resolved (but the alias is)
-// import {tester as tester2} from './client/components/core/utils';
-
 import td from 'testdouble';
 
 describe('demo tests', () => {
-    beforeEach(() => {
-        const x = td.replace('./client/components/core/utils').tester;
 
-        // Not picking up the alias so enable this to see the problem
-        // const y = td.replace('sheep');
+  afterEach(() => {
+    td.reset();
+  })
 
-        // This isn't overwriting the 'sheep' version'
-        td.when(x()).thenReturn('foo');
-    });
+  it('should pass with relative path from root', () => {
+    const x = td.replace('./client/components/core/utils').tester;
+    td.when(x()).thenReturn('foo');
+    const tester = require('./client/components/core/utils').tester;
+    expect(tester()).to.equal('foo');
+  });
 
-    afterEach(() => {
-        td.reset();
-    })
+  it('should pass with 2 relative path', () => {
+    const x = td.replace('./core/utils').tester;
+    td.when(x()).thenReturn('foo');
+    const tester = require('./core/utils').tester;
+    expect(tester()).to.equal('foo');
+  });
 
-    it('should pass', () => {
-        expect(1).to.equal(1);
-        // This is what I would like to mock out via td
-        expect(tester()).to.equal('bah');
-    });
+  it('should pass with relative path and alias', () => {
+    const x = td.replace('./core/utils').tester;
+    td.when(x()).thenReturn('foo');
+    const tester = require('sheep').tester;
+    expect(tester()).to.equal('foo');
+  });
 
-    it('should fail', () => {
-        expect(1).not.to.equal(2);
-    })
+  it('should pass with alias and alias', () => {
+    const x = td.replace('core/utils').tester;
+    td.when(x()).thenReturn('foo');
+    const tester = require('core/utils').tester;
+    expect(tester()).to.equal('foo');
+  });
 });
